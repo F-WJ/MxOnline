048c3fe69dee53c8b5804827b21dc2f050b33527
F-WJ <364580936@qq.com>
2017-05-22 22:59:19 +0800

    测试2

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,12 +3,23 @@ __author__ = 'FWJ'
 __date__ = '2017-05-01 22:40'
 
 import xadmin
-
-from .models import EmailVerifyRecord
+from .models import EmailVerifyRecord, Banner
 
 
 # object为python最顶层的类,不能继承admin类
 class EmailVerifyRecordAdmin(object):
-    pass
+    # 设置显示内容
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 搜索和导出
+    search_fields = ['code', 'email', 'send_type']
+    # 过滤器
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    list_dislay = ['title', 'image', 'url', 'index', 'add_time']
+    search_fieds = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
