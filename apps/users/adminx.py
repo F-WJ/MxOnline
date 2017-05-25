49cbaefe91763ffa7d8090b1d7513d9ab0577a70
F-WJ <364580936@qq.com>
2017-05-25 21:07:05 +0800

    xadmin全局配置

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,7 +3,23 @@ __author__ = 'FWJ'
 __date__ = '2017-05-01 22:40'
 
 import xadmin
+from xadmin import views
+
 from .models import EmailVerifyRecord, Banner
+
+
+# xadmin全局配置
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+class GlobalSettings(object):
+    # 设置后台左上角信息
+    site_title = "慕学后台管理系统"
+    # 后台底部信息
+    site_footer = "慕学在线网"
+    # 右边菜单管理
+    menu_style = "accordion"
 
 
 # object为python最顶层的类,不能继承admin类
@@ -23,3 +39,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
